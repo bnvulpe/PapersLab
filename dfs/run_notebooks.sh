622de226e7321ef4da5f270e555b5e5c441271ba
bnvulpe <b.vulpe@alumnos.upm.es>
2024-05-23 09:41:50 +0200

    automatized elasticsearch import, added queries.ipynb file for use to do queries in Elasticsearch and edited docker-configuration template to delete the databases commented

--- dfs/run_notebooks.sh
+++ dfs/run_notebooks.sh
@@ -17,3 +17,20 @@ wait
 jupyter nbconvert --execute --to notebook --inplace "/opt/workspace/"graph.ipynb"" &
 wait
 jupyter nbconvert --execute --to notebook --inplace "/opt/workspace/"renaming.ipynb"" &
+
+# Wait for all background processes to finish
+wait
+echo "All data has been extracted successfully! You can go on and build the databases."
+
+# Wait for Elasticsearch to be available
+while ! curl -s http://elasticsearch:9200 >/dev/null; do
+  echo "Waiting for Elasticsearch..."
+  sleep 5
+done
+
+echo "Elasticsearch is up and running!"
+
+# Run the initialization notebook
+jupyter nbconvert --execute --to notebook --inplace "/opt/workspace/"init_elasticdb.ipynb"" 
+
+echo "Imported data to Elasticsearch successfully! You can access the /elasticsearch_queries in localhost:8889 to run some queries."
